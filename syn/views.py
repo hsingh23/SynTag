79b30bd220151bb48f01168d3ab2e33c3320a363
Harsh Singh <hsingh23@illinois.edu>
2013-03-16 01:04:20 -0500

    feat: add tagged-text endpoint to home view
    Accept a `t` parameter via GET or POST in the syn home view and
    return the output of syn.tagged. Keep the existing `s` POST
    handling unchanged, return 404 when neither param is present.
    Also refresh syn-pos-tagger.pkl and add a root-level copy.

--- syn/views.py
+++ syn/views.py
@@ -1,13 +1,18 @@
-from syn import do_it_all
+from syn import do_it_all, tagged
 from django.http import HttpResponse
 from json import dumps
 from django.views.decorators.csrf import csrf_exempt
 
+
 @csrf_exempt
 def home(request):
     p = request.POST
+    r = request.REQUEST
     if "s" in p:
         res = do_it_all(p["s"])
+        return HttpResponse(res)
+    elif "t" in r:
+        res = tagged(r['t'])
         return HttpResponse(res)
     else:
         return HttpResponse(status=404)
